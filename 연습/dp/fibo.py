3d86e81cf84a9651081090bd6a3aa05df3c3aef8
김재현 <kjhkk1020@naver.com>
2023-01-18 16:34:48 +0900

    동적프로그래밍 피보나치 수열 함수

---
+++ 연습/dp/fibo.py
@@ -0,0 +1,20 @@
+# 한번 계산되었던 결과는 저장
+d = [0] * 100
+
+def fibo(x):
+    global d
+
+    # 피보나치 수열의 첫 번째, 두 번째 숫자는 1
+    if x == 1 or x == 2:
+        return 1
+
+    # 이미 계산한 적이 있는 숫자라면 저장되어있는 숫자 반환    
+    if d[x] != 0:
+        return d[x]
+    
+    # 계산한 적이 없는 숫자라면 계산해서 저장하고 반환(Memoization)
+    else:
+        d[x] = fibo(x - 1) + fibo(x - 2)
+        return d[x]
+    
+print(fibo(5))
