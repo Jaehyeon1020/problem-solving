31082f80f8958bfec7b77b8f46d443910d8c2f8c
김재현 <kjhkk1020@naver.com>
2023-01-18 16:54:35 +0900

    실행시간 측정 코드

---
+++ 연습/실행시간측정.py
@@ -0,0 +1,17 @@
+import time
+
+###### 입력 ######
+
+#################
+
+start_time = time.time() # 실행시간측정
+
+###### 실행 ######
+
+
+
+
+#################
+
+end_time = time.time() # 실행시간측정
+print("실행시간:", end_time - start_time, "초") # 실행시간측정
