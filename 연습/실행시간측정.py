9f97c0b2620175c46eb67b51043dae5160c45629
Jaehyeon1020 <kjhkk1020@naver.com>
2023-02-09 11:50:40 +0900

    실행시간 측정 코드 변경

--- 연습/실행시간측정.py
+++ 연습/실행시간측정.py
@@ -1,17 +1,15 @@
-import time
+import time as _time
 
 ###### 입력 ######
 
 #################
 
-start_time = time.time() # 실행시간측정
+start_time = _time.time()  # 실행시간측정
 
 ###### 실행 ######
 
 
-
-
 #################
 
-end_time = time.time() # 실행시간측정
-print("실행시간:", end_time - start_time, "초") # 실행시간측정
+end_time = _time.time()  # 실행시간측정
+print("실행시간:", end_time - start_time, "초")  # 실행시간측정
